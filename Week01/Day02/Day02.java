fb694e39fcb81266989c4a6e9ec0adbe19c722c0
Guilherme dos Santos <gs92dev@gmail.com>
2024-08-29 18:48:25 -0500

    Refactor Day02.java to improve code readability and add a new method for printing a custom message

--- Week01/Day02/Day02.java
+++ Week01/Day02/Day02.java
@@ -1,6 +1,17 @@
+/*
+ * Name: Guilherme dos Santos
+ * Date: 8/29/2024
+ * Lesson 2 Demo
+ */
 public class Day02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Day 2");
+        hello();
+    }
+
+    public static void hello() {
+        String name = "Hello World";
+        System.out.println(name);
     }
 }
