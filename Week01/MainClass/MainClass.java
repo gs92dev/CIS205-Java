d0df2c04861309ce2b9db9c99c15d8615c6af3e8
Guilherme dos Santos <gs92dev@gmail.com>
2024-08-29 19:03:25 -0500

    feat: Add three quotes about programming to MainClass

--- Week01/MainClass/MainClass.java
+++ Week01/MainClass/MainClass.java
@@ -1,6 +1,25 @@
+/*
+ * Task 1:
+ * Name: Guilherme dos Santos
+ * Date: 8/29/2024
+ * In class Activity 1
+ * This is the in class activity of the semester where we're supposed to print three quotes about programming.
+ */
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("My name is John Doe.");
+        // Quote 1, the could should be concise
+        System.out.println(
+                "The code your write makes you a programmer. The code you delete makes you a good one. The code you don't have to write makes you a great one. ");
+        System.out.println("Mario Fusco, principal software engineer at Red Hat");
+
+        // Quote 2, a code should be self-explanatory
+        System.out.println("Code is like humor. When you have to explain it, it's bad. ");
+        System.out.println("Cory House, React and JavaScript specialist");
+
+        // Quote 3, a code should be readable:
+        System.out.println(
+                "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. ");
+        System.out.println("Martin Fowler, author and speaker on software development");
+
     }
 }
