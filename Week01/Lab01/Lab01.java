61a0883ea8dec0210ea4b49522861f22540a1e53
Guilherme dos Santos <gs92dev@gmail.com>
2024-09-03 17:55:28 -0500

    chore: Update Lab01 and InClassActivities classes

---
+++ Week01/Lab01/Lab01.java
@@ -0,0 +1,27 @@
+/*
+ * Name: Guilherme dos Santos
+ * Date: 8/29/2024
+ * Lab 01
+ */
+public class Lab01 {
+
+    public static void main(String[] args) {
+        // Name
+        System.out.println("Guilherme");
+        // Student ID
+        System.out.println("00985562");
+        // Hobbies
+        System.out.println("Traveling and meeting new people");
+        // Goals after completing my degree
+        System.out.println("To get a bachelor degree in computer science and become a software engineer");
+    }
+
+}
+/*
+ * Guilherme
+ * 00985562
+ * Traveling and meeting new people
+ * Goal: To get a bachelor degree in computer science and become a software
+ * engineer
+ * [Done] exited with code=0 in 0.493 seconds
+ */
---
+++ Week02/Day01/InClassActivites.java
@@ -0,0 +1,55 @@
+/*
+ * Name: Guilherme dos Santos
+ * Date: 09/03/2024
+ * In Class Activity
+ */
+public class InClassActivites {
+    public static void main(String[] args) {
+
+        // Task 1: Create a variable for each of the following data types: int,
+        // Use appropriate variable names that follow Java naming conventions.
+        // double, char, boolean
+        int myInteger = 10;
+        double myDouble = 3.14;
+        char myChar = 'G';
+        boolean myBoolean = true;
+
+        // Task 2: Declare variables with appropriate data types based on the
+        // descriptions provided below.
+        // Description 1: Store the first letter of a person's name.
+        char firstLetterName = 'A';
+        // Description 2: Store whether a person is a student.
+        boolean isStudent = true;
+        // Description 3: Store the population of a city.
+        int cityPopulation = 1000000;
+        // Description 4: Store the average grade of a class.
+        double averageGrade = 85.5;
+        // Description 5: Store a person's full name.
+        String fullName = "Guilherme dos Santos";
+        // Description 6: Store the number of days in a week.
+        int daysInWeek = 7;
+        // Description 10: Store the price of an item in a shop.
+        double itemPrice = 19.99;
+        // Task 3: Declare a constant for the speed of light in meters per second.
+        // (299792458)
+        int SpeedOfLight = 299792458;
+        // Print the value of this constant to the console.
+        System.out.println("The speed of light is " + SpeedOfLight + " m/s");
+        // Task 4: Write an arithmetic expression that calculates the area of a
+        // /rectangle.
+        // The expression should use two variables (length and width) and print the
+        // result.
+        // Task 5: Demonstrate explicit casting from a double to an int.
+        // Print both the original double value and the casted int value.
+        // Task 6: Print a string that includes the following: a tab, a newline,
+        // and a double quote.
+        // Task 7: Create a String variable and perform the following operations:
+        // 1. Concatenate two strings.
+        // 2. Find the length of a string.
+        // Print the results of each operation.
+        // Task 8: Import the Scanner class and create a Scanner object.
+        // Use the Scanner to read an integer, a double, and a string from the
+        // user.
+        // Print each value entered by the user.
+    }
+}
