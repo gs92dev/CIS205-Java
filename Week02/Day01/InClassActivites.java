c9a8161ea872192e1a24ef9cb78da24688864367
Guilherme dos Santos <gs92dev@gmail.com>
2024-09-06 22:39:24 -0500

    Finished in class activity

--- Week02/Day01/InClassActivites.java
+++ Week02/Day01/InClassActivites.java
@@ -14,7 +14,7 @@ public class InClassActivites {
         // Use appropriate variable names that follow Java naming conventions.
         // double, char, boolean
         int myInteger = 10;
-        double myDouble = 3.14;
+        double myDouble = 3.47;
         char myChar = 'G';
         boolean myBoolean = true;
 
@@ -33,7 +33,7 @@ public class InClassActivites {
         // Description 6: Store the number of days in a week.
         int daysInWeek = 7;
         // Description 10: Store the price of an item in a shop.
-        double itemPrice = 19.99;
+        double itemPrice = 15.99;
         // Task 3: Declare a constant for the speed of light in meters per second.
         // (299792458)
         final int SPEED_OF_LIGHT = 299792458;
@@ -50,30 +50,39 @@ public class InClassActivites {
 
         // Task 5: Demonstrate explicit casting from a double to an int.
         // Print both the original double value and the casted int value.
-        double myDoubleValue = 3.14;
+        double myDoubleValue = 3.1415;
         System.out.println("Original double value: " + myDoubleValue);
-        System.out.println("Int value " + (int) myDoubleValue);
+        System.out.println("\nInt value " + (int) myDoubleValue);
 
         // Task 6: Print a string that includes the following: a tab, a newline,
         // and a double quote.
         System.out.println("Hello\tWorld\n\"Hello, World!\"");
         // Task 7: Create a String variable and perform the following operations:
+        String myName = "Guilherme";
         // 1. Concatenate two strings.
+        String myLastName = "dos Santos";
+        String MyFullNameString = myName + " " + myLastName;
         // 2. Find the length of a string.
+        int myNameLength = myName.length();
         // Print the results of each operation.
+        System.out.println("First name: " + myName);
+        System.out.println("Last name: " + myLastName);
+        System.out.println("Concatenated name: " + MyFullNameString);
+        System.out.println("The length of my name: " + myNameLength);
         // Task 8: Import the Scanner class and create a Scanner object.
         // Use the Scanner to read an integer, a double, and a string from the
         // user.
         int userInteger;
         double userDouble;
         String UserString;
-        ;
 
+        // Prompt the user to enter an integer, a double, and a string.
         System.out.println("Enter an integer: ");
         userInteger = userInput.nextInt();
 
         System.out.println("Enter a double: ");
         userDouble = userInput.nextDouble();
+
         userInput.nextLine();
         System.out.println("Enter a string: ");
         UserString = userInput.nextLine();
@@ -86,4 +95,27 @@ public class InClassActivites {
 
         userInput.close();
     }
+    /*
+     * Output:
+     * The speed of light is 299792458 m/s
+     * The area of the rectangle is 74.75 m^2
+     * Original double value: 3.1415
+     * 
+     * Int value 3
+     * Hello World
+     * "Hello, World!"
+     * First name: Guilherme
+     * Last name: dos Santos
+     * Concatenated name: Guilherme dos Santos
+     * The length of my name: 9
+     * Enter an integer:
+     * 15
+     * Enter a double:
+     * 15.5
+     * Enter a string:
+     * Guilherme
+     * The integer entered is: 15
+     * The double entered is: 15.5
+     * The string entered is: Guilherme
+     */
 }
