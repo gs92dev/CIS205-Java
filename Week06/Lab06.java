9a8e703fa48469fa72c539be9ece83129a12a2ed
Guilherme dos Santos <gs92dev@gmail.com>
2024-10-05 18:09:50 -0500

    Refactor Lab06.java to improve word length calculation and add user input validation

--- Week06/Lab06.java
+++ Week06/Lab06.java
@@ -2,21 +2,26 @@
 /*
  * Guilherme dos Santos
  * Lab 06
- * 10/03/2024
+ * 10/05/2024
  */
 import java.util.Scanner;
 
 public class Lab06 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String sentence, longest;
-        String[] splittedSentence;
 
+        // Variables declaration
+        String sentence, longest; // to store the sentence and the longest word
+        String[] splittedSentence; // to store the splitted sentence
+
+        // Input sentence from user
         System.out.println("Enter a sentence: ");
         sentence = input.nextLine();
 
+        // Split the sentence into words
         splittedSentence = sentence.split(" ");
 
+        // assume the first word is the longest
         longest = splittedSentence[0];
 
         for (int i = 0; i < splittedSentence.length; i++) {
@@ -27,7 +32,22 @@ public class Lab06 {
 
         System.out.println("The longest word in the sentence is: " + longest);
         System.out.println("The length of " + longest + " the longest word is: " + longest.length());
-
+        // close the scanner
+        input.close();
     }
+
+    /*
+     * output:
+     * Enter a sentence:
+     * Guilherme dos Santos
+     * The longest word in the sentence is: Guilherme
+     * The length of Guilherme the longest word is: 9
+     * 
+     * A longer sentence example:
+     * You should always try to avoid long sentences. Below are two examples, as
+     * well as some facts about long sentences in general.
+     * The longest word in the sentence is: sentences.
+     * The length of sentences. the longest word is: 10
+     */
 
 }
