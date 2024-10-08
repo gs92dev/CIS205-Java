0d15df80c678ae4e18695772397cd928c9392d97
Guilherme dos Santos <gs92dev@gmail.com>
2024-10-08 18:23:08 -0500

    Add inClass.java file with welcome message

---
+++ Week07/inClass.java
@@ -0,0 +1,15 @@
+/*
+ * Guilherme
+ * Class 10/08/2024
+ * 
+ */
+
+public class inClass {
+    public static void printWelcome() {
+        System.out.println("Welcome to the class");
+    }
+
+    public static void main(String[] args) {
+        printWelcome();
+    }
+}
