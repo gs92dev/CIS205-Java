fbdc45be55516242f79cab29d1b181780346e61c
Guilherme dos Santos <gs92dev@gmail.com>
2024-10-08 20:17:23 -0500

    Refactor inClass.java to add welcome message with user's name and additional math functions

--- Week07/inClass.java
+++ Week07/inClass.java
@@ -5,11 +5,26 @@
  */
 
 public class inClass {
-    public static void printWelcome() {
-        System.out.println("Welcome to the class");
+    public static void printWelcome(String name) {
+        System.out.println("Welcome to the class " + name);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double add(double a, double b) {
+        double sum = a + b;
+        return sum;
     }
 
     public static void main(String[] args) {
-        printWelcome();
+        String name = "Guilherme";
+        printWelcome(name);
+
+        int num1 = 5;
+        int num2 = 6;
+        System.out.println(add(num1, num2));
+        System.out.println(add(5.5, 6.6));
     }
 }
