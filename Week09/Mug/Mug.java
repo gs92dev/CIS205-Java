8a00386c422a30ff6573a4223e53ce5eadfe907e
Guilherme dos Santos <gs92dev@gmail.com>
2024-10-26 17:43:15 -0500

    Refactor Mug class to add constructors and methods for managing mug attributes and actions

--- Week09/Mug/Mug.java
+++ Week09/Mug/Mug.java
@@ -8,7 +8,45 @@ package Mug;
 
 public class Mug {
 
-    public static void main(String[] args) {
+    private String color;
+    private int capacity;
+    private boolean isFull;
 
+    public Mug(String color, int capacity) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void fill() {
+        if (!this.isFull) {
+            System.out.println("The mug is now full.");
+        } else {
+            this.isFull = true;
+            System.out.println("The mug is already full.");
+        }
+    }
+
+    public void empty() {
+        if (this.isFull) {
+            this.isFull = false;
+            System.out.println("The mug is now empty.");
+        } else {
+            System.out.println("The mug is already empty.");
+        }
     }
 }
